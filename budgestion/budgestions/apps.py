17ce93a10ad6dc9b0bdd71377fa7b182b013acf7
ebarnabo <edwinbarnabot@gmail.com>
2023-06-27 16:46:12 -0400

    Correction projet Django

--- budgestion/budgestions/apps.py
+++ budgestion/budgestions/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BudgestionConfig(AppConfig):
+class BudgestionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'budgestion'
+    name = 'budgestions'
